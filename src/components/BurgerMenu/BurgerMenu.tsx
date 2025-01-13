1039040d2a709a3b46906ecfc8b5a22558c478f0
--global <--global>
2025-01-13 21:28:02 +0100

    move burger menu css to dedicated file

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./BurgerMenu.css";
 
 const BurgerMenu: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -52,61 +53,6 @@ const BurgerMenu: React.FC = () => {
           </div>
         </div>
       </div>
-      <style>
-        {`
-          .burger-menu {
-            position: fixed;
-            top: 54px;
-            left: 0;
-            width: 100vw;
-            height: 100vh;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            z-index: 1009;
-          }
-          .burger-menu.open {
-            transform: translateY(0);
-          }
-          .menu-content {
-            text-align: center;
-            width: 100%;
-            height: 100%;
-          }
-          .links {
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-            padding-top: 20vh;
-            padding-inline: 20vw;
-            justify-content: flex-start;
-            gap: 10px;
-          }
-          .links a {
-            padding: 10px 0;
-            text-decoration: none;
-            color: #333;
-            font-size: 18px;
-            width: 100%;border-inline: 16px solid transparent;
-            transition:
-              border-bottom-color 0.3s ease-in-out,
-              border-inline-color 0.3s ease-in-out;
-          }
-          .links a.active {
-            font-weight: bold;
-            border-bottom: 3px solid var(--pumpkin-orange);
-            border-inline: 16px solid transparent;
-          }
-          .links a:hover {
-            border-bottom: 3px solid var(--pumpkin-orange);
-            border-inline: 16px solid transparent;
-            transition:
-              border-bottom-color 0.3s ease-in-out,
-              border-inline-color 0.3s ease-in-out;
-          }
-        `}
-      </style>
     </>
   );
 };
